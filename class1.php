b22e5b7f30a2dab1205e604ea7811fc96cca3420
unknown <jamesdanieltiller@protonmail.com>
2020-05-18 09:56:34 +1200

    v0.0.2

--- class1.php
+++ class1.php
@@ -1,5 +1,11 @@
 <?php
+
 echo "Hello World";
+
+echo "<h1>Hello World</h1>";
 
 echo " Success";
 
+echo "<p>Hello World</p>";
+
+echo "<p> style='color: red'>Hello World</p>";
--- helloworld.php
+++ helloworld.php
@@ -1,10 +1,4 @@
 <?php
 echo "Hello World";
 
-echo "<h1>Hello World</h1>";
-
 echo " Success";
-
-echo "<p>Hello World</p>";
-
-echo "<p> style='color: red'>Hello World</p>";
