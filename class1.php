cc2a6031457e09b9aa8226987cd85f649abe1664
unknown <jamesdanieltiller@protonmail.com>
2020-05-18 10:16:33 +1200

    v0.0.4

--- class1.php
+++ class1.php
@@ -8,7 +8,7 @@ echo " Success";
 
 echo "<p>Hello World</p>";
 
-echo "<p> style='color: red'>Hello World</p>";
+echo "<p style='color: red'>Hello World</p>";
 
 $i = 0; //set counter
 while ($i < 100){
