fc813b6cb3cd6ebb716adfd32c5330b848210204
unknown <jamesdanieltiller@protonmail.com>
2020-05-19 10:09:11 +1200

    Have a good *

--- class2_2.php
+++ class2_2.php
@@ -5,3 +5,14 @@ date_default_timezone_set('Pacific/Auckland');
 $t = date("Y-m-d"); //https://www.php.net/manual/en/function.date.php
 $t1 = date("H:i:s");
 echo $t." ".$t1;
+
+$hour = date("H")
+$minutes = date("i");
+
+if ($hour<"10" && $minutes < "10"){
+    echo "<p>"."Have a good morning"."</p>";
+}elseif ($hour<"20"){
+    echo "<p>"."Have a good day"."</p>"
+}else {
+    echo "<p>"."Have a good night"."</p>"
+}
