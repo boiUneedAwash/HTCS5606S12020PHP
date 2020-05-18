f6ac2234b725e94f081cc0e0dee63b7600736667
unknown <jamesdanieltiller@protonmail.com>
2020-05-19 10:11:41 +1200

    Have a good * fix 1

--- class2_2.php
+++ class2_2.php
@@ -6,13 +6,13 @@ $t = date("Y-m-d"); //https://www.php.net/manual/en/function.date.php
 $t1 = date("H:i:s");
 echo $t." ".$t1;
 
-$hour = date("H")
+$hour = date("H");
 $minutes = date("i");
 
 if ($hour<"10" && $minutes < "10"){
     echo "<p>"."Have a good morning"."</p>";
 }elseif ($hour<"20"){
-    echo "<p>"."Have a good day"."</p>"
+    echo "<p>"."Have a good day"."</p>";
 }else {
-    echo "<p>"."Have a good night"."</p>"
+    echo "<p>"."Have a good night"."</p>";
 }
