398fd4ef2fd4cdb23e4eeb72e5e665ea8a48a23f
unknown <jamesdanieltiller@protonmail.com>
2020-05-21 09:12:53 +1200

    phpMyAdmin Folder

---
+++ calculate.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST["width"])) { //isset checks the post data
+    $w = $_POST["width"];
+    $h = $_POST["height"];
+    $a = calculateArea($w, $h); // corresponding to my function input width and height
+    echo "Area: " . $a;
+} else {
+    echo "go back to form page";
+}
+
+
+//$area = $w * $h; // formula
+//f(x,y) = x * y
+function calculateArea($width, $height)
+{ //parameters for input
+    $area = $width * $height;
+    return $area;
+}
