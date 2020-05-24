72790ca809ba8da9e0ad235fdfd966395ef90f6c
unknown <jamesdanieltiller@protonmail.com>
2020-05-25 09:41:21 +1200

    week5 start and folder restructure

---
+++ week6/showrecords.php
@@ -0,0 +1,12 @@
+<?php
+$server = "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user = "dt7if5fn5w2fs141";
+$pwd = "rmycl3gp6mqjkscj";
+$database = "yvj349jjrtc4w5h1";
+
+$connection = new mysqli($server, $user, $pwd, $database);
+if ($connection->connect_error){
+    echo $connection->connect_error;
+}else{
+    echo "Connection Created";
+}
