5f0f54587c2169c8c6b137a5b3b89e9ca5b5b29a
unknown <jamesdanieltiller@protonmail.com>
2020-05-26 11:23:27 +1200

    login html and php added

---
+++ week6/login.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Author: James Tiller
+ * Date 26th of May 2020
+ * Version 1.0
+ * Purpose for login
+ */
+if (isset($_POST["username"])){
+    $username = $_POST["username"];
+    $password - $_POST["password"];
+    $server = "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $user = "dt7if5fn5w2fs141";
+    $pwd = "rmycl3gp6mqjkscj";
+    $database = "yvj349jjrtc4w5h1";
+
+    $connection = new mysqli($server, $user, $pwd, $database);
+    if ($connection->connect_error){
+        echo $connection->connect_error;
+    }else{
+        echo "Connection Created ";
+    }
+
+    // is the username in my table
+    $sql = "select * from Users where username = '$username'"; //this is our query
+    $result = $connection->query($sql); //run query on this connection through method query()
+    if ($result->num_rows == 1){ // means user exist in our database
+        while ($row = $result->fetch_assoc()){
+            if ($row["password"] == $password){ //check password
+                echo "access granted";
+            }else{
+                echo "wrong password";
+            }
+        }
+    } else{
+        echo "wrong username";
+    }
+
+
+}else{
+    ?>
+    <script>
+        window.open("loginform.html","_self"); // go to login form
+    </script>
+    <?php
+}
